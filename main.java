c3c8e6cdd3c99a0aa37dfb47f5bc59e0c0b12832
asteraceae <58607819+asteraceae@users.noreply.github.com>
2022-01-08 20:47:21 -0600

    something to start w

---
+++ main.java
@@ -0,0 +1,17 @@
+/*
+Send file to another device using bluetooth
+command line select file
+similar to ssh copy
+
+1. find nearby device
+    problem: how to establish a connection? with a device that has the application installed as well
+2. select device
+3. select file
+    establish a download folder on target (permissions needed?)
+4. send file
+*/
+public static void main(String args[]){
+
+
+
+}
